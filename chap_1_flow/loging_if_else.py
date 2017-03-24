30d1f4b2fe33cc25f9f231a2c9eaaf6a6ed279c7
naughtron <cnaughton@dekaresearchcom>
2017-03-24 19:20:26 -0400

    adding flow_example

---
+++ chap_1_flow/loging_if_else.py
@@ -0,0 +1,9 @@
+username = input('Please enter your username: ')
+password = input('Please enter your password: ')
+
+if username == 'naughtron':
+  print('hey there ' + username)
+  if password == 'p4ssw0rd':
+    print('access granted!')
+  else:
+    print('Nope!')
